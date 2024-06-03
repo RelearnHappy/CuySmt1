e11c3156e3fa1630dfc4eed639290460d41cbb00
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2024-06-04 01:08:59 +0700

    Add files via upload

---
+++ 06 - JS/index2.js
@@ -0,0 +1,25 @@
+const mahasiswa = [
+    {
+        nama: "azidin suryadi",
+        alamat: "kampung inggris",
+        usia: 29,
+    },
+    {
+        nama: "a suryadi",
+        alamat: "kampung inggris",
+        usia: 29,
+    }
+    ]
+    
+    function getDetailData() {
+        // mahasiswa.map(function (result, i) {
+        //     console.table(result)
+        // })
+    
+        // mahasiswa.forEach(result => {
+        //     console.table(result)        
+        // });
+        
+        // console.log(`data yang anda cari adalah:${mahasiswa.alamat}`)
+    }
+    
